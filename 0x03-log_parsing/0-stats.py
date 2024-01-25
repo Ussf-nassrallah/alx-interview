d7e7871f3346e3abf47144e0ca6b52f2ca6d79bb
Youssef Nasrallah <67008509+Ussf-nassrallah@users.noreply.github.com>
2024-01-25 15:43:17 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,82 +1,49 @@
 #!/usr/bin/python3
-''' document '''
-import re
+''' 0. Log parsing: solution '''
+
+import sys
 
 
-def extract_input(input_line):
-    '''
-    document
-    '''
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
+def helper(codes, size):
+    '''helper function'''
+    print("File size: {}".format(size))
+    sorted_codes = sorted(codes.items())
+
+    for key, value in sorted_codes:
+        if value is not 0:
+            print(f"{key}: {value}")
+
+
+if __name__ == "__main__":
+    codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
     }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
 
+    size = 0
+    counter = 0
 
-def print_statistics(total_file_size, status_codes_stats):
-    '''document
-    '''
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
-
-
-def update_metrics(line, total_file_size, status_codes_stats):
-    '''document
-    '''
-    line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
-        status_codes_stats[status_code] += 1
-    return total_file_size + line_info['file_size']
-
-
-def run():
-    '''document
-    '''
-    line_num = 0
-    total_file_size = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
     try:
-        while True:
-            line = input()
-            total_file_size = update_metrics(
-                line,
-                total_file_size,
-                status_codes_stats,
-            )
-            line_num += 1
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
-    except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_codes_stats)
-
-
-if __name__ == '__main__':
-    run()
+        for result in sys.stdin:
+            counter += 1
+            info = result.split()
+            try:
+                size += int(info[-1])
+            except:
+                pass
+            try:
+                codes[info[-2]] += 1
+            except:
+                pass
+            if counter % 10 is 0:
+                helper(codes, size)
+        helper(codes, size)
+    except KeyboardInterrupt:
+        helper(codes, size)
+        raise
