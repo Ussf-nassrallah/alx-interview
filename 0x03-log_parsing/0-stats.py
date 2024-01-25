19077ad2c4e07d2e901198424af996572ed1dce8
Youssef Nasrallah <67008509+Ussf-nassrallah@users.noreply.github.com>
2024-01-25 15:35:47 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,43 +7,31 @@ import sys
 def helper(codes, size):
     '''helper function'''
     print("File size: {}".format(size))
-    sorted_codes = sorted(codes.items())
-
-    for key, value in sorted_codes:
-        if value is not 0:
+    for key, value in sorted(codes.items()):
+        if value != 0:
             print(f"{key}: {value}")
 
 
 if __name__ == "__main__":
-    codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
-
-    size = 0
-    counter = 0
-
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
+    file_size = 0
+    counts = 0
     try:
-        for result in sys.stdin:
-            counter += 1
-            info = result.split()
+        for line in sys.stdin:
+            counts += 1
+            data = line.split()
             try:
-                size += int(info[-1])
+                file_size += int(data[-1])
             except:
                 pass
             try:
-                codes[info[-2]] += 1
+                status_codes[data[-2]] += 1
             except:
                 pass
-            if counter % 10 is 0:
-                helper(codes, size)
-        helper(codes, size)
+            if counts % 10 == 0:
+                print_stats(status_codes, file_size)
+        print_stats(status_codes, file_size)
     except KeyboardInterrupt:
-        helper(codes, size)
+        print_stats(status_codes, file_size)
         raise
