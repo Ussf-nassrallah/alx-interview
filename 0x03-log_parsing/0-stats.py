705987433811858024e92ba24412cac436a84e6c
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-25 16:04:42 +0100

    fix: task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -17,33 +17,34 @@ def helper(inp):
     return False
 
 
-try:
-    obj = {}
-    size = 0
+if __name__ == "__main__":
+    try:
+        obj = {}
+        size = 0
 
-    for idx, line in enumerate(stdin, start=1):
-        line = line.strip()
+        for idx, line in enumerate(stdin, start=1):
+            line = line.strip()
 
-        if not helper(line):
-            continue
+            if not helper(line):
+                continue
 
-        slices = line.split(" ")
-        size += int(slices[-1])
+            slices = line.split(" ")
+            size += int(slices[-1])
 
-        if slices[-2] not in obj:
-            obj[slices[-2]] = 1
-        else:
-            obj[slices[-2]] += 1
+            if slices[-2] not in obj:
+                obj[slices[-2]] = 1
+            else:
+                obj[slices[-2]] += 1
 
-        obj = dict(sorted(obj.items()))
+            obj = dict(sorted(obj.items()))
 
-        if idx % 10 == 0:
-            print(f"File size: {size}")
-            for key, value in obj.items():
-                print(f"{key}: {value}")
-except Exception as err:
-    pass
-finally:
-    print("File size: {}".format(size))
-    for key, value in obj.items():
-        print("{}: {}".format(key, value))
+            if idx % 10 == 0:
+                print(f"File size: {size}")
+                for key, value in obj.items():
+                    print(f"{key}: {value}")
+    except Exception as err:
+        pass
+    finally:
+        print("File size: {}".format(size))
+        for key, value in obj.items():
+            print("{}: {}".format(key, value))
